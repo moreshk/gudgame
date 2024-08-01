9631e1f8f84dc80a21ad1dc5083ed586cd66c612
moreshk <moreshk@gmail.com>
2024-08-01 16:19:44 +0530

    got rid of comments

--- app/rps-bet/[id]/page.tsx
+++ app/rps-bet/[id]/page.tsx
@@ -33,13 +33,16 @@ export default function RPSBetDetails() {
   const [isResolving, setIsResolving] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const wallet = useWallet();
-  const isResolved = bet?.winner_address !== null || bet?.winnings_disbursement_signature !== null;
+  const isResolved =
+    bet?.winner_address !== null ||
+    bet?.winnings_disbursement_signature !== null;
 
   const getResultMessage = () => {
-    if (!bet || !wallet.publicKey) return '';
-    if (bet.winner_address === 'DRAW') return 'It\'s a draw!';
-    if (bet.winner_address === wallet.publicKey.toBase58()) return 'You won! 🎉';
-    return 'You lost 😢';
+    if (!bet || !wallet.publicKey) return "";
+    if (bet.winner_address === "DRAW") return "It's a draw!";
+    if (bet.winner_address === wallet.publicKey.toBase58())
+      return "You won! 🎉";
+    return "You lost 😢";
   };
 
   useEffect(() => {
@@ -89,8 +92,10 @@ export default function RPSBetDetails() {
     if (bet) {
       const formattedAmount = Number(bet.bet_amount * 2).toFixed(2);
       const tweetText = `Play me in Rock Paper Scissors @gudgamelol ! Winner gets ${formattedAmount} SOL - ${window.location.href}`;
-      const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`;
-      window.open(twitterUrl, '_blank');
+      const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+        tweetText
+      )}`;
+      window.open(twitterUrl, "_blank");
     }
   };
 
@@ -101,13 +106,13 @@ export default function RPSBetDetails() {
         <h1 className="text-3xl font-bold mb-8 text-center">
           Rock Paper Scissor Game Details 
         </h1> */}
-        <main className="flex-grow container mx-auto px-4 py-8">
-      {/* <h1 className="text-3xl font-bold mb-8 text-center">
+      <main className="flex-grow container mx-auto px-4 py-8">
+        {/* <h1 className="text-3xl font-bold mb-8 text-center">
         Rock Paper Scissor Game by {bet ? formatAddress(bet.bet_maker_address) : 'Loading...'}
       </h1> */}
-       <h1 className="text-3xl font-bold mb-8 text-center">
+        <h1 className="text-3xl font-bold mb-8 text-center">
           {isLoading ? (
-            'Loading...'
+            "Loading..."
           ) : bet ? (
             isResolved ? (
               getResultMessage()
@@ -120,12 +125,12 @@ export default function RPSBetDetails() {
                   className="text-blue-400 hover:text-blue-300"
                 >
                   {formatAddress(bet.bet_maker_address)}
-                </a>{' '}
+                </a>{" "}
                 has started a game of Rock Paper Scissors
               </>
             )
           ) : (
-            'Game not found'
+            "Game not found"
           )}
         </h1>
         {isLoading && <p className="text-center">Loading Game details...</p>}
@@ -142,112 +147,71 @@ export default function RPSBetDetails() {
         )}
         {bet && !isResolving && (
           <>
-          <div className="bg-gray-800 rounded-lg shadow-lg p-6 max-w-2xl mx-auto">
-            {/* <h2 className="text-2xl font-semibold mb-4">Game #{bet.id}</h2> */}
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <div>
-                {/* <p className="font-semibold">Game Amount:</p>
+            <div className="bg-gray-800 rounded-lg shadow-lg p-6 max-w-2xl mx-auto">
+              {/* <h2 className="text-2xl font-semibold mb-4">Game #{bet.id}</h2> */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div>
+                  {/* <p className="font-semibold">Game Amount:</p>
                 <p>{bet.bet_amount} SOL</p> */}
-                <p className="font-semibold mb-2">Game Amount: {Number(bet.bet_amount).toFixed(2)} SOL</p>
-
-              </div>
-              <div>
-  <p className="font-semibold flex items-center justify-start md:justify-end">
-    <span className="mr-2">Pot Address:</span>
-    <a
-      href={`https://solscan.io/account/${bet.pot_address}`}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="text-blue-400 hover:text-blue-300"
-    >
-      {formatAddress(bet.pot_address)}
-    </a>
-  </p>
-</div>
-              {/* <div>
-                <p className="font-semibold">Maker Address:</p>
-                <a
-                  href={`https://solscan.io/account/${bet.bet_maker_address}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-blue-400 hover:text-blue-300 break-all"
-                >
-                  {formatAddress(bet.bet_maker_address)}
-                </a>
-              </div> */}
-              {/* <div>
-                <p className="font-semibold">Bet Making Time:</p>
-                <p>{formatDate(bet.bet_making_timestamp)}</p>
-              </div> */}
-              {bet.bet_taker_address && (
-                <>
-                  <div>
-                    <p className="font-semibold">Taker Address:</p>
+                  <p className="font-semibold mb-2">
+                    Game Amount: {Number(bet.bet_amount).toFixed(2)} SOL
+                  </p>
+                </div>
+                <div>
+                  <p className="font-semibold flex items-center justify-start md:justify-end">
+                    <span className="mr-2">Pot Address:</span>
                     <a
-                      href={`https://solscan.io/account/${bet.bet_taker_address}`}
+                      href={`https://solscan.io/account/${bet.pot_address}`}
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="text-blue-400 hover:text-blue-300 break-all"
+                      className="text-blue-400 hover:text-blue-300"
                     >
-                      {formatAddress(bet.bet_taker_address)}
+                      {formatAddress(bet.pot_address)}
                     </a>
-                  </div>
-                  <div>
-                    <p className="font-semibold">Taker Move:</p>
-                    <p>{bet.taker_bet || "Not revealed"}</p>
-                  </div>
-                  {/* <div>
-                    <p className="font-semibold">Taking Time:</p>
-                    <p>
-                      {bet.bet_taking_timestamp
-                        ? formatDate(bet.bet_taking_timestamp)
-                        : "N/A"}
-                    </p>
-                  </div> */}
-                </>
-              )}
-              {bet.winner_address && (
-                <>
-                  <div>
-                    <p className="font-semibold">Winner:</p>
-                    <a
-                      href={`https://solscan.io/account/${bet.winner_address}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-blue-400 hover:text-blue-300 break-all"
-                    >
-                      {formatAddress(bet.winner_address)}
-                    </a>
-                  </div>
-                  {bet.winnings_disbursement_signature && (
+                  </p>
+                </div>
+               
+                {bet.winner_address && (
+                  <>
                     <div>
-                      <p className="font-semibold">Winnings Disbursement:</p>
+                      <p className="font-semibold">Winner:</p>
                       <a
-                        href={`https://solscan.io/tx/${bet.winnings_disbursement_signature}`}
+                        href={`https://solscan.io/account/${bet.winner_address}`}
                         target="_blank"
                         rel="noopener noreferrer"
                         className="text-blue-400 hover:text-blue-300 break-all"
                       >
-                        {formatSignature(bet.winnings_disbursement_signature)}
+                        {formatAddress(bet.winner_address)}
                       </a>
                     </div>
-                  )}
-                </>
+                    {bet.winnings_disbursement_signature && (
+                      <div>
+                        <p className="font-semibold">Winnings Disbursement:</p>
+                        <a
+                          href={`https://solscan.io/tx/${bet.winnings_disbursement_signature}`}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-blue-400 hover:text-blue-300 break-all"
+                        >
+                          {formatSignature(bet.winnings_disbursement_signature)}
+                        </a>
+                      </div>
+                    )}
+                  </>
+                )}
+              </div>
+
+              {!isResolved && ( // Only render BetOptions if the bet is not resolved
+                <BetOptions
+                  betId={bet.id}
+                  betAmount={bet.bet_amount}
+                  potAddress={bet.pot_address}
+                  onBetPlaced={handleBetPlaced}
+                  isResolved={isResolved} // Pass the new prop
+                />
               )}
             </div>
-            
-            {!isResolved && ( // Only render BetOptions if the bet is not resolved
-              <BetOptions
-                betId={bet.id}
-                betAmount={bet.bet_amount}
-                potAddress={bet.pot_address}
-                onBetPlaced={handleBetPlaced}
-                isResolved={isResolved} // Pass the new prop
-              />
-            )}
-              
-          </div>
-          <div className="mt-6 text-center">
+            <div className="mt-6 text-center">
               <button
                 onClick={shareOnTwitter}
                 className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded inline-flex items-center"
@@ -257,7 +221,8 @@ export default function RPSBetDetails() {
               </button>
             </div>
             <p className="mt-4 text-center text-sm text-gray-400">
-              Choose your move and match the game amount. Whoever wins gets the pot! <br />
+              Choose your move and match the game amount. Whoever wins gets the
+              pot! <br />
               Winnings are automatically sent to the winning wallet address.
             </p>
           </>
