eadad240789747834789f2ddf9ad0d2af991b3a5
moreshk <moreshk@gmail.com>
2024-08-09 11:33:45 +0530

    removed login

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,9 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const isAuthenticated = request.cookies.get('isAuthenticated');
-  if (!isAuthenticated && request.nextUrl.pathname !== '/login') {
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|login).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
