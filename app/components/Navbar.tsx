6bccbd337be02b6074a74b7191998c80e198710f
moreshk <moreshk@gmail.com>
2024-08-16 16:52:36 +0530

    removed extra navbar links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -52,25 +52,6 @@ const Navbar = () => {
         // <div className="absolute top-16 right-0 w-64 bg-gray-800 rounded-md shadow-lg py-1">
                   <div className="absolute top-16 right-0 w-64 bg-gray-900 rounded-md shadow-lg py-1">
           <Link href="/create-rps-game" className="block py-2 px-4 hover:bg-gray-700">Create</Link>
-          <Link href="/open-rps-games" className="block py-2 px-4 hover:bg-gray-700">Open Games</Link>
-
-          <div>
-            <button 
-              onClick={() => setShowMyBets(!showMyBets)}
-              className="w-full text-left py-2 px-4 hover:bg-gray-700"
-            >
-              My Games
-            </button>
-            {showMyBets && (
-              <div className="bg-gray-800">
-                <Link href="/made-games" className="block py-2 px-8 hover:bg-gray-600">Made Games</Link>
-                <Link href="/taken-games" className="block py-2 px-8 hover:bg-gray-600">Taken Games</Link>
-              </div>
-            )}
-          </div>
-
-          <Link href="/leaderboard" className="block py-2 px-4 hover:bg-gray-700">Leaderboard</Link>
-          <Link href="https://docs.gudgame.lol" target="_blank" rel="noopener noreferrer" className="block py-2 px-4 hover:bg-gray-700">Docs</Link>
         </div>
       )}
     </nav>
