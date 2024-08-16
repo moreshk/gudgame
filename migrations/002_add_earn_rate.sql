38293ecff1eb889defce4c5efa1ff77d0b6ab5a9
moreshk <moreshk@gmail.com>
2024-08-16 16:09:35 +0530

    changed migration scripts to only add column if it doesnt already exist

--- migrations/002_add_earn_rate.sql
+++ migrations/002_add_earn_rate.sql
@@ -1,3 +1,9 @@
--- Add earn_rate column to user_balances table
-ALTER TABLE user_balances
-ADD COLUMN earn_rate INTEGER NOT NULL DEFAULT 1;
+-- Add earn_rate column to user_balances table if it doesn't exist
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.columns 
+                   WHERE table_name='user_balances' AND column_name='earn_rate') THEN
+        ALTER TABLE user_balances
+        ADD COLUMN earn_rate INTEGER NOT NULL DEFAULT 1;
+    END IF;
+END $$;
